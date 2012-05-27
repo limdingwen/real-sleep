3e3cfc20797632cf6d6e2692c1dbcf8570f0487a
Lim DingWen <limdingwen@gmail.com>
2012-05-27 11:14:16 +0800

    v0.2 #15

--- RealSleep/src/com/github/limdingwen/RealSleep/Help.java
+++ RealSleep/src/com/github/limdingwen/RealSleep/Help.java
@@ -6,15 +6,15 @@ import org.bukkit.entity.Player;
 
 public class Help {
 	public static void printHelp(CommandSender sender) {
-		sender.sendMessage(ChatColor.GREEN + "==RealSleep Help - Commands==");
-		sender.sendMessage("----------------------");
-		sender.sendMessage("Remember that you could replace all 'sleep' with 'rs'.");
-		sender.sendMessage("----------------------");
-		sender.sendMessage("/sleep - Base of all commands.");
-		sender.sendMessage("/sleep help - Displays this help screen.");
-		sender.sendMessage("/sleep check - Checks your sleep.");
-		sender.sendMessage("/sleep check <Player> - Checks another person's sleep.");
-		sender.sendMessage("/sleep restore <Player> - Base of all commands.");
-		sender.sendMessage("----------------------");
+		sender.sendMessage(ChatColor.GREEN + "=====" + ChatColor.RESET + "RealSleep Help - Commands" + ChatColor.GREEN + "=====");
+		sender.sendMessage(ChatColor.LIGHT_PURPLE + "----------------------");
+		sender.sendMessage(ChatColor.BOLD + "Remember that you could replace all 'sleep' with 'rs' or 'realsleep'.");
+		sender.sendMessage(ChatColor.LIGHT_PURPLE + "----------------------");
+		sender.sendMessage(ChatColor.GOLD + "/sleep" + ChatColor.BLUE + " - Base of all commands.");
+		sender.sendMessage(ChatColor.GOLD + "/sleep help" + ChatColor.BLUE + " - Displays this help screen.");
+		sender.sendMessage(ChatColor.GOLD + "/sleep check" + ChatColor.BLUE + " - Checks your sleep.");
+		sender.sendMessage(ChatColor.GOLD + "/sleep check <Player>" + ChatColor.BLUE + " - Checks another person's sleep.");
+		sender.sendMessage(ChatColor.GOLD + "/sleep restore <Player>" + ChatColor.BLUE + " - Restores a person's sleep to full.");
+		sender.sendMessage(ChatColor.LIGHT_PURPLE + "----------------------");
 	}
 }
